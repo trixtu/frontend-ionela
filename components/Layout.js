e0dc7a3ccbb38aa45ad55c70125aebc0bfbd15fb
trixtu <trixtu12@gmail.com>
2023-09-28 21:44:10 +0200

    update

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './footer'
+import Head from 'next/head'
 
 export default function Layout({ children, slider }) {
   return (
--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="google" content="nositelinkssearchbox" key="sitelinks" />
+        <meta name="google" content="notranslate" key="notranslate" />
+        <meta name="robots" content="noindex,nofollow" />
+        <meta name="robots" content="all" />
+      </Head>
       <body>
         <Main />
         <NextScript />
