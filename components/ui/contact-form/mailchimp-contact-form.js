c0790d19626e8b39026bcf23aa52a668f7a2babe
trixtu <trixtu12@gmail.com>
2023-10-29 10:08:16 +0100

    update

--- components/ui/contact-form/mailchimp-contact-form.js
+++ components/ui/contact-form/mailchimp-contact-form.js
@@ -1,25 +1,26 @@
 import { sanitize } from '@/utils/miscellaneous'
+import { toast } from 'react-toastify'
 import {
+  Alert,
   Button,
   FormControl,
   FormControlLabel,
-  FormLabel,
   Radio,
   RadioGroup,
 } from '@mui/material'
 import React from 'react'
 
 export default function MailchimpContactForm({ status, message, onValidated }) {
-  const [value, setValue] = React.useState(null)
-  const [name, setName] = React.useState(null)
-  const [prenume, setPrenume] = React.useState(null)
-  const [dataNastere, setDataNastere] = React.useState(null)
-  const [email, setEmail] = React.useState(null)
-  const [telefon, setTelefon] = React.useState(null)
-  const [textarea, setTextarea] = React.useState(null)
+  const [value, setValue] = React.useState('')
+  const [name, setName] = React.useState('')
+  const [prenume, setPrenume] = React.useState('')
+  const [dataNastere, setDataNastere] = React.useState('')
+  const [email, setEmail] = React.useState('')
+  const [telefon, setTelefon] = React.useState('')
+  const [textarea, setTextarea] = React.useState('')
 
   const [error, setError] = React.useState(null)
-
+  const [messageForm, setMessageForm] = React.useState(null)
   /**
    * Handle form submit.
    *
@@ -29,27 +30,83 @@ export default function MailchimpContactForm({ status, message, onValidated }) {
   const handleFormSubmit = () => {
     setError(null)
 
-    if (!email) {
-      setError('Please enter a valid email address')
+    if (!value) {
+      setError('value')
+      setMessageForm('This field is required')
       return null
+    } else {
+      setMessageForm(null)
+    }
+
+    if (!name) {
+      setError('name')
+      setMessageForm('Name ist required.')
+      return null
+    } else {
+      setMessageForm(null)
+    }
+
+    if (!prenume) {
+      setError('prenume')
+      setMessageForm('Lastname ist required.')
+      return null
+    } else {
+      setMessageForm(null)
+    }
+
+    if (!dataNastere) {
+      setError('data-nastere')
+      setMessageForm('This field ist required.')
+      return null
+    } else {
+      setMessageForm(null)
+    }
+
+    if (!email) {
+      setError('email')
+      setMessageForm('Email ist required.')
+      return null
+    } else {
+      setMessageForm(null)
     }
 
     if (!telefon) {
-      setError('This field is required.')
+      setError('telefon')
+      setMessageForm('Phone ist required.')
+      return null
+    } else {
+      setMessageForm(null)
+    }
+
+    if (!textarea) {
+      setError('textarea')
+      setMessageForm('This field ist required.')
+      return null
+    } else {
+      setMessageForm(null)
     }
 
     const isFormValidated = onValidated({
+      MMERGE6: value,
       EMAIL: email,
       PHONE: telefon,
       FNAME: prenume,
       LNAME: name,
-      MMERGE6: value,
       MMERGE7: textarea,
       MMERGE8: dataNastere,
     })
 
     // On success return true
-    return email && email.indexOf('@') > -1 && isFormValidated
+    if (status === 'success') {
+      setValue('')
+      setName('')
+      setPrenume('')
+      setDataNastere('')
+      setEmail('')
+      setTelefon('')
+      setTextarea('')
+    }
+    return value && email && email.indexOf('@') > -1 && isFormValidated
   }
 
   /**
@@ -90,27 +147,37 @@ export default function MailchimpContactForm({ status, message, onValidated }) {
 
   return (
     <section className="mb-8 mt-6">
-      {'error' === status || error ? (
-        <div
-          className="text-red-700 pt-2"
-          dangerouslySetInnerHTML={{ __html: error || getMessage(message) }}
-        />
-      ) : null}
-      {'success' === status && error !== status && !error && (
-        <div
-          className="text-green-200 font-bold pt-2"
-          dangerouslySetInnerHTML={{ __html: sanitize(message) }}
-        />
+      {
+        'success' === status &&
+          error !== status &&
+          !error &&
+          // <div
+          //   className="text-green-200 font-bold pt-2"
+          //   dangerouslySetInnerHTML={{ __html: sanitize(message) }}
+          // />
+          toast.success(
+            <div dangerouslySetInnerHTML={{ __html: sanitize(message) }} />
+          )
+        // <Alert severity="success">
+        //   <div dangerouslySetInnerHTML={{ __html: sanitize(message) }} />
+        // </Alert>
+      }
+      {'error' === status && (
+        <Alert severity="error">
+          <div dangerouslySetInnerHTML={{ __html: sanitize(message) }} />
+        </Alert>
       )}
       <FormControl>
         <label id="demo-radio-buttons-group-label">
-          Cum iti pot fi de folos?<span>*</span>
+          Cum iti pot fi de folos?
+          <span className="ml-1 text-red-600 text-lg font-semibold">*</span>
         </label>
         <RadioGroup
           aria-labelledby="demo-radio-buttons-group-label"
           name="radio-buttons-group"
           value={value}
           onChange={(event) => setValue(event.target.value)}
+          onKeyUp={(event) => handleInputKeyEvent(event)}
         >
           <FormControlLabel
             value="Sedinta de consultanta numerologica 1:1"
@@ -133,68 +200,122 @@ export default function MailchimpContactForm({ status, message, onValidated }) {
             label="Instruire numerologica de baza"
           />
         </RadioGroup>
+        {error === 'value' ? (
+          <span className="text-red-700">{messageForm}</span>
+        ) : null}
       </FormControl>
       <div className="grid grid-cols-2 gap-4">
         <div>
           <label id="nume-form">
-            Nume<span>*</span>
+            Nume
+            <span className="ml-1 text-red-600 text-lg font-semibold">*</span>
           </label>
           <input
             id="nume-form"
             type="text"
             value={name}
+            className={error === 'name' ? 'border-red-600 mb-0' : null}
             onChange={(event) => setName(event.target.value)}
+            onKeyUp={(event) => handleInputKeyEvent(event)}
           />
+          {error === 'name' ? (
+            <span className="text-red-700">{messageForm}</span>
+          ) : null}
         </div>
         <div>
           <label id="prenume-form">
-            Prenume<span>*</span>
+            Prenume
+            <span className="ml-1 text-red-600 text-lg font-semibold">*</span>
           </label>
           <input
+            className={error === 'prenume' ? 'border-red-600 mb-0' : null}
             id="prenume-form"
             type="text"
             value={prenume}
             onChange={(event) => setPrenume(event.target.value)}
+            onKeyUp={(event) => handleInputKeyEvent(event)}
           />
+          {error === 'prenume' ? (
+            <span className="text-red-700">{messageForm}</span>
+          ) : null}
         </div>
       </div>
-      <label id="data-nastere">
-        ZIUA - LUNA - ANUL nasterii<span>*</span>
-      </label>
-      <input
-        id="data-nastere"
-        type="text"
-        value={dataNastere}
-        onChange={(event) => setDataNastere(event.target.value)}
-      />
-      <label id="email">
-        Email<span>*</span>
-      </label>
-      <input
-        id="email"
-        type="email"
-        value={email}
-        onChange={(event) => setEmail(event.target.value)}
-        onKeyUp={(event) => handleInputKeyEvent(event)}
-      />
-      <label id="telefon">
-        Telefon<span>*</span>
-      </label>
-      <input
-        id="telefon"
-        type="text"
-        value={telefon}
-        onChange={(event) => setTelefon(event.target.value)}
-      />
-      <label id="textarea">
-        Povestește-mi pe scurt situatia la care doresti sa lucrăm împreună
-        <span>*</span>
-      </label>
-      <textarea
-        id="textarea"
-        value={textarea}
-        onChange={(event) => setTextarea(event.target.value)}
-      />
+      <div>
+        <label id="data-nastere">
+          ZIUA - LUNA - ANUL nasterii
+          <span className="ml-1 text-red-600 text-lg font-semibold">*</span>
+        </label>
+        <input
+          id="data-nastere"
+          type="text"
+          className={error === 'data-nastere' ? 'border-red-600 mb-0' : null}
+          value={dataNastere}
+          onChange={(event) => setDataNastere(event.target.value)}
+          onKeyUp={(event) => handleInputKeyEvent(event)}
+        />
+        {error === 'data-nastere' ? (
+          <span className="text-red-700">{messageForm}</span>
+        ) : null}
+      </div>
+      <div>
+        <label id="email">
+          Email
+          <span className="ml-1 text-red-600 text-lg font-semibold">*</span>
+        </label>
+        <input
+          className={
+            error === 'email'
+              ? 'border-red-600 mb-0 focus:border-red-600'
+              : null
+          }
+          id="email"
+          type="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          onKeyUp={(event) => handleInputKeyEvent(event)}
+        />
+        {error === 'email' ? (
+          <div
+            className="text-red-700"
+            dangerouslySetInnerHTML={{
+              __html: error || getMessage(message),
+            }}
+          />
+        ) : null}
+      </div>
+      <div>
+        <label id="telefon">
+          Telefon
+          <span className="ml-1 text-red-600 text-lg font-semibold">*</span>
+        </label>
+        <input
+          id="telefon"
+          type="text"
+          className={error === 'telefon' ? 'border-red-600 mb-0' : null}
+          value={telefon}
+          onChange={(event) => setTelefon(event.target.value)}
+          onKeyUp={(event) => handleInputKeyEvent(event)}
+        />
+        {error === 'telefon' ? (
+          <span className="text-red-700">{messageForm}</span>
+        ) : null}
+      </div>
+      <div>
+        <label id="textarea">
+          Povestește-mi pe scurt situatia la care doresti sa lucrăm împreună
+          <span className="ml-1 text-red-600 text-lg font-semibold">*</span>
+        </label>
+        <textarea
+          id="textarea"
+          className={error === 'textarea' ? 'border-red-600 mb-0' : null}
+          value={textarea}
+          onChange={(event) => setTextarea(event.target.value)}
+          onKeyUp={(event) => handleInputKeyEvent(event)}
+        />
+        {error === 'textarea' ? (
+          <span className="text-red-700">{messageForm}</span>
+        ) : null}
+      </div>
       <Button variant="contained" color="gold" onClick={handleFormSubmit}>
         Trimite Programare
       </Button>
