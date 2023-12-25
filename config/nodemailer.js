376b83f00848f717f9ba0873301c9697eab01b9e
trixtu <trixtu12@gmail.com>
2023-12-25 15:54:41 +0100

    update

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer'
 
 const email = process.env.EMAIL
 const pass = process.env.EMAIL_PASS
-const emailto= process.env.EMAIL_TO
+const emailTo= process.env.EMAIL_TO
 
 export const transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -14,5 +14,5 @@ export const transporter = nodemailer.createTransport({
 
 export const mailOptions = {
   from: email,
-  to: emailto,
+  to: emailTo,
 }
